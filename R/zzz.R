e0f7e5fbbbebcf6806e8c50eb44ae7d17477f3c5
Charles <cescapecs@gmail.com>
2020-09-07 21:11:28 +0100

    rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("lhm version 1.0.0.14 (2020-09-05 12:11:55)")
+    packageStartupMessage("lhm version 1.0.0.15 (2020-09-07 21:10:08)")
 }
