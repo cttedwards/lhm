2f9210c91dbe1f82e87dd14134139220242bf037
Charles <cescapecs@gmail.com>
2020-09-19 13:05:58 +1200

    Update prior.R

--- R/prior.R
+++ R/prior.R
@@ -22,8 +22,36 @@ setMethod("initialize", "prior", function(.Object, x) {
     
     if (missing(x)) .Object@.Data <- numeric()
     else {
-        if (length(x) > 1) .Object@.Data <- as.numeric(x)
-        else               .Object@.Data <- numeric(x)
+        if (length(x) > 1) {
+        
+            x <- as.numeric(x)
+        
+            .Object@.Data <- x
+            
+            # calculate log-normal pars
+            
+            # transform to normal
+            y <- log(x)
+            
+            # estimate parameters of
+            # normal distribution log(x)
+            mu     <- mean(y)
+            sigma  <- sd(y)
+            sigma2 <- sigma^2
+            
+            # estimate parameters of
+            # log-normal distribution
+            theta <- exp(mu + sigma2/2)
+            nu    <- exp(2*mu + sigma2)*(exp(sigma2) - 1)
+            cv    <- sqrt(exp(sigma2) - 1)
+            
+            # assign
+            .Object@lognormal.par <- list('E[log(r)]' = mu, 'SD[log(r)]' = sigma, 'E[r]' = theta, 'VAR[r]' = nu, 'CV[r]' = cv)
+            
+        } else {
+            .Object@.Data <- numeric(x)
+        }
     }
-    .Object
+    return(.Object)
 })
+
